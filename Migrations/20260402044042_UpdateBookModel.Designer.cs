47d4b9bcb681dc24b0f0d27e30cd3fed634bab74
emigdio <emigdiocamacho@gmail.com>
2026-04-02 00:41:09 -0400

    feat: add PdfKey property to Book model and update database schema

--- Migrations/20260402044042_UpdateBookModel.Designer.cs
+++ Migrations/20260402044042_UpdateBookModel.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PdfLibraryApi.Data;
 
@@ -10,9 +11,11 @@ using PdfLibraryApi.Data;
 namespace PdfLibraryApi.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260402044042_UpdateBookModel")]
+    partial class UpdateBookModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.5");
---
+++ Migrations/20260402044042_UpdateBookModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PdfLibraryApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateBookModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PdfKey",
+                table: "Books",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PdfKey",
+                table: "Books");
+        }
+    }
+}
